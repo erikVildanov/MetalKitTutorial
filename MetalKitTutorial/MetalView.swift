ee7ff4bb2e769907c7899f9b66a0cf1c68250212
Erik Vildanov <erik.v@cloudberrylab.com>
2019-08-02 13:03:14 +0300

    ch03

--- MetalKitTutorial/MetalView.swift
+++ MetalKitTutorial/MetalView.swift
@@ -9,7 +9,23 @@
 import UIKit
 import MetalKit
 
+//vector_float4 x = 1.0f;         // x = { 1, 1, 1, 1 }.
+//
+//vector_float3 y = { 1, 2, 3 };  // y = { 1, 2, 3 }.
+//
+//x.xyz = y.zyx;                  // x = { 1/3, 1/2, 1, 1 }.
+//
+//x.w = 0;                        // x = { 1/4, 1/3, 1/2, 0 }.
+
+struct Vertex {
+    var position: vector_float4
+    var color: vector_float4
+}
+
 class MetalView: MTKView {
+    
+    var vertex_buffer: MTLBuffer!
+    var rps: MTLRenderPipelineState! = nil
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
@@ -17,33 +33,41 @@ class MetalView: MTKView {
     
     func render() {
         device = MTLCreateSystemDefaultDevice()
-        
-        let vertex_data:[Float] = [-1.0, -1.0, 0.0, 1.0,
-                                   1.0, -1.0, 0.0, 1.0,
-                                   0.0,  1.0, 0.0, 1.0]
-        let data_size = vertex_data.count * MemoryLayout<Float>.size
-        let vertex_buffer = device!.makeBuffer(bytes: vertex_data, length: data_size, options: [])
-        
+        createBuffer()
+        registerShaders()
+        sendToGPU()
+    }
+    
+    func createBuffer() {
+        let vertex_data = [Vertex(position: [-1.0, -1.0, 0.0, 1.0], color: [1, 0, 0, 1]),
+                           Vertex(position: [ 1.0, -1.0, 0.0, 1.0], color: [0, 1, 0, 1]),
+                           Vertex(position: [ 0.0,  1.0, 0.0, 1.0], color: [0, 0, 1, 1])]
+        vertex_buffer = device!.makeBuffer(bytes: vertex_data, length: MemoryLayout<Vertex>.size * 3, options:[])
+    }
+    
+    func registerShaders() {
         let library = device!.makeDefaultLibrary()!
         let vertex_func = library.makeFunction(name: "vertex_func")
         let frag_func = library.makeFunction(name: "fragment_func")
-        
         let rpld = MTLRenderPipelineDescriptor()
         rpld.vertexFunction = vertex_func
         rpld.fragmentFunction = frag_func
         rpld.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
-        let rps = try! device!.makeRenderPipelineState(descriptor: rpld)
-        
+        do {
+            try rps = device!.makeRenderPipelineState(descriptor: rpld)
+        } catch let error {
+            print("\(error)")
+        }
+    }
+    
+    func sendToGPU() {
         if let rpd = currentRenderPassDescriptor, let drawable = currentDrawable {
-            rpd.colorAttachments[0].clearColor = MTLClearColorMake(0, 0.5, 0.5, 1.0)
+            rpd.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1.0)
             let command_buffer = device!.makeCommandQueue()!.makeCommandBuffer()
             let command_encoder = command_buffer!.makeRenderCommandEncoder(descriptor: rpd)
-            
             command_encoder!.setRenderPipelineState(rps)
             command_encoder!.setVertexBuffer(vertex_buffer, offset: 0, index: 0)
             command_encoder!.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
-            
             command_encoder!.endEncoding()
             command_buffer!.present(drawable)
             command_buffer!.commit()
